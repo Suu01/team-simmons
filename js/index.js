daddb07180a2feda4827df180ceec8ae689babcc
Suu01 <ksj19990823@daum.net>
2022-06-06 21:54:44 +0900

    menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // 기능 추가
+function openSearch() {
+    document.getElementById('menu-sub').style.display = 'block';
+}
+
+function closeSearch() {
+    document.getElementById('menu-sub').style.display = 'none';
+}
